412233e2bffbd7da38f8044003edd505a4e9f6dc
electroabir <77103394+electroabir@users.noreply.github.com>
2021-01-08 12:14:19 +0600

    unfixed

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-$(function () {
-    $(document).scroll(function () {
-        // var $navp = $("#fixed-nav-product");
-        var $nav = $("#fixed-nav");
-        var $address = $("#menu-container");
+// $(function () {
+//     $(document).scroll(function () {
+//         // var $navp = $("#fixed-nav-product");
+//         var $nav = $("#fixed-nav");
+//         var $address = $("#menu-container");
 
-        $nav.toggleClass("scrolled", $(this).scrollTop() > 200);
-        // $navp.toggleClass("scrolled", $(this).scrollTop() > $navp.height());
+//         $nav.toggleClass("scrolled", $(this).scrollTop() > 200);
+//         // $navp.toggleClass("scrolled", $(this).scrollTop() > $navp.height());
 
 
-    });
-});
+//     });
+// });
