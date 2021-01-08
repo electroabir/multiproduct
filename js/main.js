52f530897d6f1c1c39d095dc251877db699e4259
electroabir <77103394+electroabir@users.noreply.github.com>
2021-01-08 12:09:39 +0600

    Initial Deploy

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function () {
+    $(document).scroll(function () {
+        // var $navp = $("#fixed-nav-product");
+        var $nav = $("#fixed-nav");
+        var $address = $("#menu-container");
+
+        $nav.toggleClass("scrolled", $(this).scrollTop() > 200);
+        // $navp.toggleClass("scrolled", $(this).scrollTop() > $navp.height());
+
+
+    });
+});
